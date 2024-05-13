44e2d08ed0e98417f5f54b7cb1e8b017f2561cad
deepspacesonja <75392165+BlurbyTheSlurp@users.noreply.github.com>
2024-05-13 10:18:52 +0200

    ..

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+function toggle_light_mode() {
+  var app = document.getElementsByTagName("BODY")[0];
+  if (localStorage.lightMode == "dark") {
+localStorage.lightMode = "light";
+app.setAttribute("light-mode", "light");
+  } else {
+localStorage.lightMode = "dark";
+app.setAttribute("light-mode", "dark");
+  }		
+}
+
+var app = document.getElementsByTagName("BODY")[0];
+    if (localStorage.lightMode == "dark") {
+        app.setAttribute("light-mode", "dark");
+    }
+
+    window.addEventListener("storage", function () {
+      if (localStorage.lightMode == "dark") {
+          app.setAttribute("light-mode", "dark");
+    } else {
+        app.setAttribute("light-mode", "light");
+    }
+  }, false);
