20442f7cb0ef49ca7dff7dc00d6eb71a609272d8
deepspacesonja <75392165+BlurbyTheSlurp@users.noreply.github.com>
2024-06-09 16:32:04 +0200

    ...

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-require("@babel/core").transform("code", {
-  presets: ["@babel/preset-env"],
-});
+let mybutton = document.getElementById("btn-back-to-top");
 
-let mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-function topFunction() {
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
